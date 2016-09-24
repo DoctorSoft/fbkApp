da015033b0ef52dafdd45e6c2aa8d73b166d4c78
ArcherFromGrodno <ArcherFromGrodno@gmail.com>
2016-09-24 22:53:07 +0300

    The fucking proxy works!!!!! ))))

--- FaceBook/FaceBook/Program.cs
+++ FaceBook/FaceBook/Program.cs
@@ -1,20 +1,63 @@
-﻿using InputData.Implementation;
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Drawing2D;
+using System.Threading;
+using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Interactions.Internal;
+using OpenQA.Selenium.PhantomJS;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Support.UI;
+using SpecBind.Selenium;
 
 namespace FaceBook
 {
     public class Program
     {
+
         public static void Main(string[] args)
         {
-            var driver = new ChromeDriver();
-            var service = new FaceBookService();
+            var proxyUsername = "movses21";
+            var proxyPassword = "2V1G10W6Xy";
+            var proxyAddress = "5.101.64.137";
+            var proxyPort = "34530";
 
-            var inpuDataProvider = new InputDataProvider("usersDB.xlsx");
+            PhantomJSDriverService service = PhantomJSDriverService.CreateDefaultService();
+
+            service.Proxy = string.Format("{0}:{1}", proxyAddress, proxyPort);
+            service.ProxyAuthentication = string.Format("{0}:{1}", proxyUsername, proxyPassword);
+            service.ProxyType = "http";
+            service.DiskCache = true;
+            service.LoadImages = false;
+            service.WebSecurity = false;
+            service.HideCommandPromptWindow = true;
+            service.IgnoreSslErrors = true;
+            service.WebSecurity = false;
+            service.LocalToRemoteUrlAccess = true;
+            service.LoadImages = false;
+
+            var driver = new PhantomJSDriver();
+
+
+            driver.Navigate().GoToUrl("https://2ip.ru/");
+
+            Thread.Sleep(3000);
+
+            var f = driver.FindElements(By.Id("d_clip_button"));
+
+
+            Thread.Sleep(3000);
+
+
+            Thread.Sleep(10000);
+            //var service = new FaceBookService();
+
+            /*var inpuDataProvider = new InputDataProvider("usersDB.xlsx");
 
             var userList = service.GetRegistrationUserData(inpuDataProvider);
 
-            service.Registration(driver, userList.usersData);
+            service.Registration(driver, userList.usersData);*/
         }
     }
 }
